366935a87a139d3bd7b29ff3533da18073c0241d
sdmalfian <sadamalfianp@gmail.com>
2023-06-16 21:17:08 +0700

    release v.0.1 - project submission

---
+++ resources/views/index.blade.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Welcome to JamuIN</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            margin: 0;
+            padding: 0;
+            background-color: #f6f6f6;
+        }
+
+        .container {
+            max-width: 800px;
+            margin: 0 auto;
+            padding: 20px;
+            text-align: center;
+        }
+
+        .logo {
+            margin-bottom: 20px;
+        }
+
+        h1 {
+            color: #333333;
+            font-size: 36px;
+            font-weight: bold;
+            margin-bottom: 20px;
+        }
+
+        .tagline {
+            color: #555555;
+            font-size: 24px;
+            font-weight: bold;
+            margin-bottom: 20px;
+        }
+
+        .description {
+            color: #666666;
+            font-size: 16px;
+            margin-bottom: 40px;
+        }
+
+        .footer {
+            color: #999999;
+            font-size: 12px;
+            margin-top: 40px;
+        }
+    </style>
+</head>
+
+<body>
+    <div class="container">
+        <div class="logo">
+            <img src="assets/jamuin_logo.png" alt="JamuIN Logo" width="200">
+        </div>
+
+        <h1>Welcome to JamuIN API</h1>
+
+        <div class="tagline">
+            A unique marketplace for local jamu products
+        </div>
+
+        <div class="description">
+            <p>JamuIN is a dedicated marketplace that supports the growth and sale of jamu products from UMKM (Micro,
+                Small, and Medium Enterprises).</p>
+            <p>Discover a wide variety of local jamu products and experience the richness of traditional Indonesian
+                herbal remedies.</p>
+        </div>
+
+        <div>
+            <h2>Link To Documentation</h2>
+            <a href="https://documenter.getpostman.com/view/14170928/2s93sc6DNG">
+                <h3>Postman Documenter</h3>
+            </a>
+        </div>
+
+        <div class="footer">
+            &copy; 2023 JamuIN. All rights reserved
+            <p>Bangkit Academy 2023 - Product Capstone Project</p>
+        </div>
+    </div>
+</body>
+
+</html>
